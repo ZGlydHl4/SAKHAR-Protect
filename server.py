3183a4b3d4025c2b802b1358dd237e764dea2d75
Alexis <alexislecuyer14@gmail.com>
2021-06-20 15:15:56 +0200

    scan file

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from werkzeug.utils import secure_filename
 import subprocess
 import json
+import os
 
 cmd_template = "python ./VxAPI/vxapi.py"
+UPLOAD_FOLDER = "./uploads"
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/quick-scan-url", methods=['POST'])
@@ -26,6 +30,20 @@ def quick_scan_url_file():
     result = subprocess.run(cmd, capture_output=True, text=True)
     stdout = result.stdout
     stderr = result.stderr
+    return getScanResult(stdout)
+
+
+@app.route("/quick-scan-file", methods=['POST'])
+def quick_scan_file():
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    cmd = cmd_template + " scan_file --no-share-third-party 1 --allow-community-access 0 ./uploads/" + \
+        filename + " scan_metadefender"
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    stdout = result.stdout
+    stderr = result.stderr
+    os.remove("./uploads/" + filename)
     return getScanResult(stdout)
 
 
